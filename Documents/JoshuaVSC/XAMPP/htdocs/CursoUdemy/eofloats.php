83134779d6cba157b016c9e9329f4f1dd1687efb
Kaleb <volkyt.gamer790@gmail.com>
2024-06-25 08:54:06 -0300

    mais ints, floats e vars

---
+++ Documents/JoshuaVSC/XAMPP/htdocs/CursoUdemy/eofloats.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 1.2345;
+echo"<br>";
+echo 12345;
+echo "<BR>";
+echo "umdoistreisquartocinco";
---
+++ Documents/JoshuaVSC/XAMPP/htdocs/CursoUdemy/eoquee.php
@@ -0,0 +1,15 @@
+<?php
+
+$b1 = 1.2345;
+$b2 = 12345;
+$b3 = "umdoistreisquartocinco";
+echo $b1;
+echo"<br>";
+echo $b2;
+echo "<BR>";
+echo $b3;
+echo"<br>";
+
+if (is_float($b1)){
+    echo "O primeiro é float.";
+}
