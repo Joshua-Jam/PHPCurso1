e71ed9fc1359e043506ea27f7d68e64866e82d7a
Kaleb <volkyt.gamer790@gmail.com>
2024-09-11 12:26:05 -0300

    vmaos

--- Documents/JoshuaVSC/XAMPP/htdocs/CursoUdemy/DAO/001.php
+++ Documents/JoshuaVSC/XAMPP/htdocs/CursoUdemy/DAO/001.php
@@ -13,18 +13,26 @@ class Sql extends PDO {
     private function setParams($statement, $parameters = array()) {
 
         foreach ($parameters as $key => $value) {
-            
+
             $statement->bindParam($key, $value);
     }
 
     private function setParam($statement, $key, $value){
         
+        $statemente->bindParam($key, $value);
+
     }
 
 
     public function query($rawQuery, $params = array()){
 
         $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt->fetchALL(PDO::FETCH_ASSOC);
         }
     }
 }
